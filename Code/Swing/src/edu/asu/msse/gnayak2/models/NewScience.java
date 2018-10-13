3c88ed25833dbca02b6bbe0f39eb3a7750c9d2d3
Dixith <dixit_k@ymail.com>
2018-10-12 21:16:50 -0700

    Add java TimeStamp as member to the NewScience class

--- Code/Swing/src/edu/asu/msse/gnayak2/models/NewScience.java
+++ Code/Swing/src/edu/asu/msse/gnayak2/models/NewScience.java
@@ -1,22 +1,33 @@
 package edu.asu.msse.gnayak2.models;
 
-import java.util.UUID;
-
 import org.json.JSONObject;
 
+import java.sql.Timestamp;
+import java.util.UUID;
+
 public class NewScience {
-	String id;
-	String title;
-	int order;
-	String link;
+	private String id;
+	private String title;
+	private int order;
+	private String link;
+	private Timestamp timestamp;
 
 	public NewScience(String newsTitle, int order, String linkMore) {
 		id = UUID.randomUUID().toString().replace("-", "");
 		title = newsTitle;
 		this.order = order;
 		link = linkMore;
+		timestamp = new Timestamp(System.currentTimeMillis());
 	}
-	
+
+	public Timestamp getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Timestamp timestamp) {
+		this.timestamp = timestamp;
+	}
+
 	public NewScience(JSONObject object) {
 		id = object.getString("id");
 		title = object.getString("title");
