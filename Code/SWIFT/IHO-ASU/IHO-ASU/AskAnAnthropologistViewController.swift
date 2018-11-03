cd4ea86d5c577094849087e1ff38c52b0c70bcaf
Vimal Menon <vimal.menon@asu.edu>
2018-11-03 00:43:44 -0700

    Task # 165, 166  |  Time taken : 2 ½  hours
    In iOS app, to solve the scrolling issue and design discrepancy when screen orientation changes from Portrait to Landscape, the pages "Ask an Anthropologist" and "Becoming Human" were re-built using UIViewController, UIScrollView, UIContentView and appropriate auto-layout constraints so that the pages are scrollable and resizable across multiple screen sizes and screen orientations. Irrespective of the screen size or orientation, the design and layout of the pages now remain consistent throughout.

--- Code/SWIFT/IHO-ASU/IHO-ASU/AskAnAnthropologistViewController.swift
+++ Code/SWIFT/IHO-ASU/IHO-ASU/AskAnAnthropologistViewController.swift
@@ -20,9 +20,7 @@ import UIKit
 
 class AskAnAnthropologistViewController: UIViewController, UITextViewDelegate, UIWebViewDelegate {
     @IBOutlet weak var askButton: UIButton!
-    
-    @IBOutlet weak var anthro: UIWebView!
-    
+
     @IBAction func visitAnthropologist(_ sender: Any) {
         
         
@@ -37,14 +35,9 @@ class AskAnAnthropologistViewController: UIViewController, UITextViewDelegate, U
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        anthro.scrollView.isScrollEnabled = true
-        self.anthro.delegate = self
-        anthro.loadRequest(URLRequest(url: URL(fileURLWithPath: Bundle.main.path(forResource: "askAnthro", ofType: "html")!)))
         
         self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
         self.navigationItem.title = "Ask An Anthropologist"
-        
 
         //toolbar
         let label = UILabel(frame: CGRect(x: CGFloat(0), y: CGFloat(0), width: CGFloat(350), height: CGFloat(21)))
@@ -55,17 +48,6 @@ class AskAnAnthropologistViewController: UIViewController, UITextViewDelegate, U
         let toolbarTitle = UIBarButtonItem(customView: label)
         let flexible = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
         self.toolbarItems = [flexible,toolbarTitle]
-    }
-    
-    @objc(webView:shouldStartLoadWithRequest:navigationType:) func webView(_ anthro: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
-        
-        if navigationType == UIWebViewNavigationType.linkClicked {
-            UIApplication.shared.openURL(request.url!)
-            return false
-        }
-        
-        return true
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
