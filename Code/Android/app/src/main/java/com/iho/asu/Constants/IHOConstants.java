8c3c7a3bccd4de7df1b42245463ad4cddcdb6421
Dixith <dixit_k@ymail.com>
2018-11-20 13:47:15 -0700

    Update URL to production

--- Code/Android/app/src/main/java/com/iho/asu/Constants/IHOConstants.java
+++ Code/Android/app/src/main/java/com/iho/asu/Constants/IHOConstants.java
@@ -5,8 +5,8 @@ package com.iho.asu.Constants;
  */
 
 public class IHOConstants {
-//    public static final String BASE_URL = "http://107.170.239.62/";
-     public static final String BASE_URL = "http://10.0.2.2:3000/";
+     public static final String BASE_URL = "http://107.170.239.62/";
+     // public static final String BASE_URL = "http://10.0.2.2:3000/";
 
     //public static final String BASE_URL = "http://ihomobileapp.com/";
 
